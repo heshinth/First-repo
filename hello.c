2baf16f854e8162a5b49e194d03ad37e19170ce1
Heshinth <heshinth@gmail.com>
2024-01-26 09:44:51 +0530

    learning c!!!
    hello world and temprature changer

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*stdio=standard input output*/
+
+int main()
+{
+    printf(" welcome \nto\n ");
+    printf("c programming");
+}
---
+++ temprature.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+float main()
+{
+    float c;/*celcius in float*/
+    float f;/*farenheit in float*/
+    c=55;
+    f=((9*c)/5+32);
+    printf ("The temprature");
+    printf (" in celcius: %f \n",c);
+    printf("After converting  to farenheit: %f",f);
+
+}
