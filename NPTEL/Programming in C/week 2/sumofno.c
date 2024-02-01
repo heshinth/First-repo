a22135a5ee150feaffa38e606a5bd37315b952d3
Heshinth <heshinth@gmail.com>
2024-02-01 20:20:04 +0530

    Started week 2

---
+++ NPTEL/Programming in C/week 2/sumofno.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main(){
+    int a;
+    int  s;
+    s=0;
+    scanf("%d",&a);
+    while (!(a==-1)){
+        scanf("%d",&a);
+        s=s+a;
+    }
+    printf("%d",s);
+}
---
+++ NPTEL/Programming in C/week 2/whileinput.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+int main() {
+    int a;
+    scanf("%d",&a);
+    while (!(a==-1)){
+        scanf("%d",&a);    
+    }
+
+}
